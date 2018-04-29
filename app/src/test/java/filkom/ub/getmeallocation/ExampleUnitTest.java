f55f1e9daaf44aab6b010095cc9be422234de217
nyamuk <asamsulfat13@gmail.com>
2018-04-29 07:15:14 +0700

    initial commit

---
+++ app/src/test/java/filkom/ub/getmeallocation/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package filkom.ub.getmeallocation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
