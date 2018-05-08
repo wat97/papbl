a736375cb736bd40c380890cf49c83c4a1214a67
nyamuk <asamsulfat13@gmail.com>
2018-05-08 16:42:26 +0700

    add writeStoragePerm, intent camera, upload img to firebase

--- app/src/main/java/filkom/ub/getmeallocation/TambahActivity.java
+++ app/src/main/java/filkom/ub/getmeallocation/TambahActivity.java
@@ -1,6 +1,17 @@
 package filkom.ub.getmeallocation;
 
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -10,15 +21,22 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -26,21 +44,29 @@ import java.util.Date;
 import filkom.ub.getmeallocation.model.MenuModel;
 import filkom.ub.getmeallocation.model.RestoranModel;
 
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
 public class TambahActivity extends AppCompatActivity {
 
     private EditText etNamamenu, etHarga, etDate, etLokasi;
     private AutoCompleteTextView actvNamaRestoran;
-    private Button btnSubmitMenu;
+    private Button btnSubmitMenu, btnSubmitImage;
+    private ImageView imageView;
 
     private ArrayList<RestoranModel> namaRestoran = new ArrayList<RestoranModel>();
     private String[] arrayNamaRestoran;
 
     private DatabaseReference databaseRestoran;
     private DatabaseReference databaseMenu;
+    private StorageReference storageReference;
 
     private ArrayAdapter<String> arrayAdapter;
 
     public static final String TAG = "tambahM";
+
+
+    private static final int TAKE_PICTURE = 1;
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +81,9 @@ public class TambahActivity extends AppCompatActivity {
         etHarga = (EditText) findViewById(R.id.et_harga);
         etDate = (EditText) findViewById(R.id.et_date);
         etLokasi = (EditText) findViewById(R.id.et_lokasi);
+        imageView = (ImageView) findViewById(R.id.imageView);
         btnSubmitMenu = (Button) findViewById(R.id.button_submit_menu);
+        btnSubmitImage = (Button) findViewById(R.id.btn_submit_iamge);
 
         btnSubmitMenu.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,9 +91,85 @@ public class TambahActivity extends AppCompatActivity {
                 insertMenu();
             }
         });
+        btnSubmitImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isStoragePermissionGranted()) {
+
+                    takePhoto();
+                }
+            }
+        });
 
         etDate.setText(setCurrentDate());
         getAllRestoran();
+    }
+
+
+    public void takePhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File photo = new File(Environment.getExternalStorageDirectory(),  "Pic.jpg");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                Uri.fromFile(photo));
+        //FileProvider.getUriForFile(TambahActivity.this, BuildConfig.APPLICATION_ID, photo));
+        imageUri = Uri.fromFile(photo);
+        //imageUri = FileProvider.getUriForFile(TambahActivity.this, BuildConfig.APPLICATION_ID, photo);
+        startActivityForResult(intent, TAKE_PICTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case TAKE_PICTURE:
+                if (resultCode == Activity.RESULT_OK) {
+                    Uri selectedImage = imageUri;
+                    getContentResolver().notifyChange(selectedImage, null);
+                    ContentResolver cr = getContentResolver();
+                    Bitmap bitmap;
+                    try {
+                        bitmap = android.provider.MediaStore.Images.Media
+                                .getBitmap(cr, selectedImage);
+
+                        imageView.setImageBitmap(bitmap);
+                        //Toast.makeText(this, selectedImage.toString(),Toast.LENGTH_LONG).show();
+
+                    } catch (Exception e) {
+                        //Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT)
+                                .show();
+                        Log.e("Camera", e.toString());
+                    }
+                }
+        }
+    }
+
+    public  boolean isStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG,"Permission is granted");
+                return true;
+            } else {
+
+                Log.v(TAG,"Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG,"Permission is granted");
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+            Log.v(TAG,"Permission: "+permissions[0]+ "was "+grantResults[0]);
+            //resume tasks needing this permission
+        }
     }
 
     private void getAllRestoran() {
@@ -118,9 +222,13 @@ public class TambahActivity extends AppCompatActivity {
                 public void onDataChange(DataSnapshot dataSnapshot) {
                     for (DataSnapshot snapshot: dataSnapshot.getChildren()) {
                         if (namaRestoranExist.equals(snapshot.child("namaRestoran").getValue(String.class))) {
-                            snapshot.getRef().child("menu").child(databaseRestoran.push().getKey()).setValue(menuModel);
+                            String key = databaseRestoran.push().getKey();
+                            snapshot.getRef().child("menu").child(key).setValue(menuModel);
                             //Toast.makeText(TambahActivity.this, snapshot.child("namaRestoran").getValue(String.class), Toast.LENGTH_SHORT).show();
                             Toast.makeText(TambahActivity.this, "Berhasil menambahkan menu", Toast.LENGTH_SHORT).show();
+
+                            uploadImage("menu", key);
+
                             goToMain();
                         }
                     }
@@ -136,11 +244,32 @@ public class TambahActivity extends AppCompatActivity {
             RestoranModel restoranModel = new RestoranModel(actvNamaRestoran.getText().toString(), etLokasi.getText().toString());
             String key = databaseRestoran.push().getKey();
             databaseRestoran.child(key).setValue(restoranModel);
-            databaseRestoran.child(key).child("menu").child(databaseRestoran.push().getKey()).setValue(menuModel);
+            databaseRestoran.child(key).child("menu").child(key).setValue(menuModel);
             Toast.makeText(this, "Berhasil menambahkan warung", Toast.LENGTH_SHORT).show();
+            uploadImage("menu", key);
             goToMain();
         }
 
+    }
+
+    private void uploadImage(String type, String key) {
+
+        storageReference = FirebaseStorage.getInstance().getReference(type + "/" + key + ".jpg");
+
+        storageReference.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Uri downkloadUrl = taskSnapshot.getUploadSessionUri();
+                        //Toast.makeText(TambahActivity.this, downkloadUrl.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(TambahActivity.this, "failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     private String setCurrentDate() {
