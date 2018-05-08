3b31217098a80e38c2d3daa69547403b2c77912b
nyamuk <asamsulfat13@gmail.com>
2018-05-08 17:18:30 +0700

    get all restoran

--- app/src/main/java/filkom/ub/getmeallocation/HomeActivity.java
+++ app/src/main/java/filkom/ub/getmeallocation/HomeActivity.java
@@ -3,12 +3,27 @@ package filkom.ub.getmeallocation;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+import filkom.ub.getmeallocation.adapter.RestoranAdapter;
+import filkom.ub.getmeallocation.model.RestoranModel;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -19,6 +34,11 @@ public class HomeActivity extends AppCompatActivity {
     private TextView textViewUserEmail;
     private Button buttonLogout, buttonTambah;
 
+    private DatabaseReference databaseRestoran;
+
+    private RecyclerView recyclerView;
+    private RestoranAdapter restoranAdapter;
+    private ArrayList<RestoranModel> restoranModels;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,16 +53,21 @@ public class HomeActivity extends AppCompatActivity {
         if(firebaseAuth.getCurrentUser() == null){
             startActivity(new Intent(this, LoginActivity.class));
             finish();
-
         }
 
         //getting current user
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
+        databaseRestoran = FirebaseDatabase.getInstance().getReference("restoran");
+
         //initializing views
         textViewUserEmail = (TextView) findViewById(R.id.textViewUserEmail);
         buttonLogout = (Button) findViewById(R.id.buttonLogout);
         buttonTambah = (Button) findViewById(R.id.buttonTambah);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        restoranAdapter = new RestoranAdapter(this);
 
         //displaying logged in user name
         textViewUserEmail.setText("Welcome "+user.getDisplayName()+" "+user.getEmail());
@@ -65,6 +90,29 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), TambahActivity.class));
+            }
+        });
+
+        getAllRestoran();
+    }
+
+    private void getAllRestoran() {
+        databaseRestoran.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                ArrayList<RestoranModel> restorans = new ArrayList<>();
+
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                    RestoranModel restoranModel = dataSnapshot1.getValue(RestoranModel.class);
+                    restorans.add(restoranModel);
+                }
+                restoranAdapter.addItem(restorans);
+                    recyclerView.setAdapter(restoranAdapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
             }
         });
     }
